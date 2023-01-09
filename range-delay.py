c64296fee525f7a3c00063d30323be312754b972
Jan Kocurek <jas.kocurek2@wp.pl>
2023-01-09 10:59:23 +0100

    code cleanup

--- range-delay.py
+++ range-delay.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-import os, re, math
+import os, re
 
 def clearData(line):
     line = re.sub('.*Range: ', '', line)
@@ -22,51 +22,24 @@ for dist in distances:
     files = sorted(os.listdir(os.getcwd() + mesurements_path_dist))
     files.append(files.pop(0))
 
-    for filename in files: #dla każdego pomiaru dalmirzowego
+    for filename in files:
         if "txt" in filename:
             delay= float(re.sub('ms.txt', '', filename)[len(dist) + 1:])
             dealys.append(delay)
             path = os.path.join(os.getcwd() + mesurements_path_dist, filename)
             with open(path, 'r') as f:
-                # n = 
                 timeout_count = 0
                 print(f)
-                for line in f.readlines(): #pomiary z uwb
+                for line in f.readlines():
                     print(line)
                     if line != '':
-                        # n += 1
-                        # pomiar = float(clearData(line).strip())
-                        # pomiar = line[10:]
-                        # delta=pomiar-wartOcekiwana
-                        # deltaKwadrat = delta * delta
                         if line.strip() == "Timed out!":
                             timeout_count += 1
 
                 timeouts.append(timeout_count)
-            
 
-    #     sigma += delta
-    #     sigmaKwadrat += deltaKwadrat
-    # mbr = sigma / n
-    # rmse = math.sqrt(sigmaKwadrat / n)
-    # dalmierze.sort()
-    # mbry.insert(dalmierze.index(wartOczekiwana), mbr)
-    # rmsy.insert(dalmierze.index(wartOczekiwana), rmse)
-
-# plt.ylabel('min bias error')
-# plt.ylabel('mbr')
-# plt.xlabel('odległość')    
-# plt.plot(dalmierze, mbry)
 
     plt.plot(dealys, timeouts, label=("Distance " + dist))
 
 plt.legend()
 plt.show()
-
-#with open(os.path.join(os.getcwd(), "3.978.txt"), 'r') as f:
-#   wyniki = []
-#   for line in f.readlines():
-#      wyniki.append(float(clearData(line).strip()))
-
-#plt.plot(wyniki)
-#plt.show()
